efe352e1c4d34d1e2ff21cdef324641c7340d15b
bik_krlvn <kelvinbirikorang@gmail.com>
2019-06-07 01:12:29 +0000

    added login ui layout

---
+++ app/src/main/java/com/wNagiesEducationalCenterj_9905/auth/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.wNagiesEducationalCenterj_9905.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wNagiesEducationalCenterj_9905.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}

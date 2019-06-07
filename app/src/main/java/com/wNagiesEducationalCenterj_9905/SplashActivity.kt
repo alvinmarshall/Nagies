6ef2f72e0788ddfb7b0552097337964817d7c6b1
bik_krlvn <kelvinbirikorang@gmail.com>
2019-06-07 01:58:16 +0000

    added splash ui

---
+++ app/src/main/java/com/wNagiesEducationalCenterj_9905/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.wNagiesEducationalCenterj_9905
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

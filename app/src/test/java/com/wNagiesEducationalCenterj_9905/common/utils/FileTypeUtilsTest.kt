057cb98b6324fb4b8dd3b4bfe7d10810e4ebfebd
bik_krlvn <kelvinbirikorang@gmail.com>
2019-07-08 16:57:03 +0000

    test added FileTypeUtils

---
+++ app/src/test/java/com/wNagiesEducationalCenterj_9905/common/utils/FileTypeUtilsTest.kt
@@ -0,0 +1,32 @@
+package com.wNagiesEducationalCenterj_9905.common.utils
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class FileTypeUtilsTest {
+
+    @Test
+    fun getPDFType() {
+        val path = "/storage/public/document/filename.pdf"
+        val actual = FileTypeUtils.getType(path)
+        val expected = "application/pdf"
+        assertEquals("get file type", expected, actual)
+    }
+
+    @Test
+    fun getImageType() {
+        val path = "/storage/public/document/filename.jpg"
+        val actual = FileTypeUtils.getType(path)
+        val expected = "image/jpeg"
+        assertEquals("get file type", expected, actual)
+    }
+
+    @Test
+    fun getOtherType() {
+        val path = "/storage/public/document/filename.other"
+        val actual = FileTypeUtils.getType(path)
+        val expected = "*/*"
+        assertEquals("get file type", expected, actual)
+    }
+}

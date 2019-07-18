3764cab546aca54626ea9d243ba8700775c0890a
bik_krlvn <kelvinbirikorang@gmail.com>
2019-07-18 00:12:23 +0000

    refactored ImagePathUtils to ServerPathUtils

--- app/src/main/java/com/wNagiesEducationalCenterj_9905/common/utils/ServerPathUtil.kt
+++ app/src/main/java/com/wNagiesEducationalCenterj_9905/common/utils/ServerPathUtil.kt
@@ -2,7 +2,7 @@ package com.wNagiesEducationalCenterj_9905.common.utils
 
 import com.wNagiesEducationalCenterj_9905.common.SERVER_URL
 
-object ImagePathUtil {
+object ServerPathUtil {
     fun setCorrectPath(path: String?): String? {
         var imageUrl: String? = null
         var extension: String?

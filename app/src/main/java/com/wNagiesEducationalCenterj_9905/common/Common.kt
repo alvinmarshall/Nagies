2d56408a39b8f55f5283bd69354a2fb48a8ac017
bik_krlvn <kelvinbirikorang@gmail.com>
2019-07-18 00:33:13 +0000

    generic lambda function for view widget added

--- app/src/main/java/com/wNagiesEducationalCenterj_9905/common/Common.kt
+++ app/src/main/java/com/wNagiesEducationalCenterj_9905/common/Common.kt
@@ -1,2 +1,5 @@
 package com.wNagiesEducationalCenterj_9905.common
 
+fun showAnyView(view: Any?, message:String?,listener:Any?,visible:Boolean = false, Func:(Any?, String?,Any?,Boolean) -> Unit){
+    Func(view,message,listener,visible)
+}

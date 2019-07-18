17027f10ef640b93374f4a7e643f7c9b269e3f82
bik_krlvn <kelvinbirikorang@gmail.com>
2019-07-18 00:37:22 +0000

    changed api url to local

--- app/src/main/java/com/wNagiesEducationalCenterj_9905/common/AppConstants.kt
+++ app/src/main/java/com/wNagiesEducationalCenterj_9905/common/AppConstants.kt
@@ -2,11 +2,12 @@ package com.wNagiesEducationalCenterj_9905.common
 
 val LOGIN_ROLE_OPTIONS = arrayListOf("Parent","Teacher")
 const val SELECTED_ROLE = "SELECTED_ROLE"
-const val INFORDAS_BASE_URL = "http://192.168.8.101:81/api/"//"http://192.168.8.100:81/api/ --- http://nagiesportal.000webhostapp.com/api/"
+const val INFORDAS_BASE_URL = "http://192.168.8.101:81/api/"//"http://192.168.8.101:81/api/ --- http://nagiesportal.000webhostapp.com/api/"
 const val DATABASE_NAME = "app_db"
 const val LOGIN_PREF = "login_in_status"
 const val USER_TOKEN = "user_token"
-const val SERVER_URL = "http://192.168.8.101:81"//"http://192.168.8.100:81  --- http://nagiesportal.000webhostapp.com"
+const val SERVER_URL = "http://192.168.8.101:81"//"http://192.168.8.101:81  --- http://nagiesportal.000webhostapp.com"
 const val USER_INFO = "user_info"
 const val PERMISSION_PREF = "permission_pref"
 const val REQUEST_EXTERNAL_STORAGE = 100
+const val REPORT_ENTITY = "report"

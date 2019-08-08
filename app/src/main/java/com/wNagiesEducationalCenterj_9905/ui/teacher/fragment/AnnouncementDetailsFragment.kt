9de4cc443bbb0f684545cafa9d3ef54bbd3adde0
bik_krlvn <kelvinbirikorang@gmail.com>
2019-08-08 09:08:57 +0000

    [update] announcement feature added

---
+++ app/src/main/java/com/wNagiesEducationalCenterj_9905/ui/teacher/fragment/AnnouncementDetailsFragment.kt
@@ -0,0 +1,53 @@
+package com.wNagiesEducationalCenterj_9905.ui.teacher.fragment
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.wNagiesEducationalCenterj_9905.R
+import com.wNagiesEducationalCenterj_9905.base.BaseFragment
+import com.wNagiesEducationalCenterj_9905.ui.teacher.viewmodel.TeacherViewModel
+import kotlinx.android.synthetic.main.fragment_announcement_details.*
+import org.jetbrains.anko.support.v4.toast
+
+class AnnouncementDetailsFragment : BaseFragment() {
+    private lateinit var teacherViewModel: TeacherViewModel
+    private var announcementId: Int? = 0
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_announcement_details, container, false)
+    }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        announcementId = arguments?.let { AnnouncementDetailsFragmentArgs.fromBundle(it).argAnnouncementId }
+        configureViewModel()
+    }
+
+    private fun configureViewModel() {
+        teacherViewModel = ViewModelProviders.of(this, viewModelFactory)[TeacherViewModel::class.java]
+        announcementId?.let {  teacherViewModel.getAnnouncementById(it) }
+        subscribeObservers()
+    }
+
+    private fun subscribeObservers() {
+
+        teacherViewModel.cachedAnnouncement.observe(viewLifecycleOwner, Observer { msg ->
+            val title = "from: ${msg.level}"
+            val sender = "name: ${msg.sender}"
+            item_title.text = title
+            item_sender.text = sender
+            item_content.text = msg.content
+        })
+    }
+
+
+}
